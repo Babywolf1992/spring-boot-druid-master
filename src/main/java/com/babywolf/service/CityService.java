bb92970f5781e9f05eb688e5918f754fc3e259db
babywolf <591138406@qq.com>
2017-03-20 10:11:00 +0800

    提交

--- src/main/java/com/babywolf/service/CityService.java
+++ src/main/java/com/babywolf/service/CityService.java
@@ -1,7 +1,38 @@
 package com.babywolf.service;
 
+import com.babywolf.entity.City;
+import com.babywolf.mapper.CityMapper;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * Created by babywolf on 17/3/19.
  */
 public class CityService {
+    @Autowired
+    private CityMapper cityMapper;
+    public List<City> getAll(City city) {
+        if (city.getPage() != null && city.getRows() != null) {
+            PageHelper.startPage(city.getPage(), city.getRows());
+        }
+        return cityMapper.selectAll();
+    }
+
+    public City getById(Integer id) {
+        return cityMapper.selectByPrimaryKey(id);
+    }
+
+    public void deleteById(Integer id) {
+        cityMapper.deleteByPrimaryKey(id);
+    }
+
+    public void save(City city) {
+        if (city.getId() != null) {
+            cityMapper.updateByPrimaryKey(city);
+        }else {
+            cityMapper.insert(city);
+        }
+    }
 }
