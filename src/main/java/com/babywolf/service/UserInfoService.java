0924f8c5591790e9a06a177c9957d46522ce02ef
babywolf <591138406@qq.com>
2017-03-20 10:30:37 +0800

    提交代码

--- src/main/java/com/babywolf/service/UserInfoService.java
+++ src/main/java/com/babywolf/service/UserInfoService.java
@@ -1,7 +1,39 @@
 package com.babywolf.service;
 
+import com.babywolf.entity.UserInfo;
+import com.babywolf.mapper.UserInfoMapper;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * Created by babywolf on 17/3/19.
  */
 public class UserInfoService {
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    public List<UserInfo> getAll(UserInfo userInfo) {
+        if (userInfo.getPage() != null && userInfo.getRows() != null) {
+            PageHelper.startPage(userInfo.getPage(), userInfo.getRows());
+        }
+        return userInfoMapper.selectAll();
+    }
+
+    public UserInfo getById(Integer id) {
+        return userInfoMapper.selectByPrimaryKey(id);
+    }
+
+    public void deleteById(Integer id) {
+        userInfoMapper.deleteByPrimaryKey(id);
+    }
+
+    public void save(UserInfo userInfo) {
+        if (userInfo.getId() != null) {
+            userInfoMapper.updateByPrimaryKey(userInfo);
+        }else {
+            userInfoMapper.insert(userInfo);
+        }
+    }
 }
