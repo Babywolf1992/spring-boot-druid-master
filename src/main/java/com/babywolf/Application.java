77f42fbb287a61a87c63a7c7aa9a28ddec9526e2
babywolf <591138406@qq.com>
2017-03-21 11:22:46 +0800

    提交代码

--- src/main/java/com/babywolf/Application.java
+++ src/main/java/com/babywolf/Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -16,5 +17,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "redirect:countries";
     }
 }
--- src/main/java/com/babywolf/controller/CountryController.java
+++ src/main/java/com/babywolf/controller/CountryController.java
@@ -1,7 +1,32 @@
 package com.babywolf.controller;
 
+import com.babywolf.entity.Country;
+import com.babywolf.service.CountryService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
 /**
  * Created by babywolf on 17/3/20.
  */
+@Controller
+@RequestMapping("/countries")
 public class CountryController {
+    @Autowired
+    private CountryService countryService;
+
+    @RequestMapping
+    public ModelAndView getAll(Country country) {
+        ModelAndView result = new ModelAndView("index");
+        List<Country> countryList = countryService.getAll(country);
+        result.addObject("pageInfo", new PageInfo<Country>(countryList));
+        result.addObject("queryParam", country);
+        result.addObject("page", country.getPage());
+        result.addObject("rows", country.getRows());
+        return result;
+    }
 }
